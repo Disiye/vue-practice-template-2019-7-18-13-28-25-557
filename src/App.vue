06fe2c89cb498a985ed32d8c6058eb9b0f0be4ac
WENGEM <emilio.wang@oocl.com>
2019-07-19 14:19:12 +0800

    complete vue practice

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="increase"> + </button>
+    <input id="input" v-model="inputNumber">
+    <button @click="decrease"> - </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+        inputNumber:0
+    }
+  },
+  methods:{
+    increase(){
+      this.inputNumber++;
+    },
+    decrease(){
+      this.inputNumber--;
+    }
+    
   }
 }
 </script>
